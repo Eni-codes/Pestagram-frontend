e5a775a7c8261144a48b9b95762155769bd2beb7
marcusdickens <marcus.r.dickens@gmail.com>
2021-06-22 14:39:59 -0400

    profile

---
+++ src/Components/Sidenav.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import { Link } from 'react-router-dom'
+
+export default class Sidenav extends Component {
+
+    render() {
+
+        return(
+            <ul className="Side-nav">
+                <Link to="/Home"><li><button>Home</button></li></Link>
+                <Link to="/Posts"><li><button>My Post</button></li></Link>
+                <Link to="/Friends"><li><button>Friends</button></li></Link>
+                <Link to="/Photos"><li><button>My Photos</button></li></Link>
+            </ul>
+        )
+    }
+
+}
